db631ee94cb3a3c085003befac985c2aa7d9836d
Madu Mahesh Kumar <rakeshmadu24@gmail.com>
2022-08-13 14:43:14 +0530

    uc9

---
+++ src/com/linkedList/LinkedListTest.java
@@ -0,0 +1,23 @@
+package com.linkedList;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+
+		LinkedListServices list = new LinkedListServices();
+		
+		list.insertAtHead(56);
+		list.append(30);
+		list.append(70);
+		list.insertBetween(30, 40);
+		list.displayLinkedList();
+
+		list.deleteNode(40);
+
+		System.out.println(" SIZE: " + list.size());
+		System.out.println(" final sequence: ");
+		list.displayLinkedList();
+		
+		System.out.println(" SIZE: " + list.size());
+	}
+}
